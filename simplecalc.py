77d71ff3ab4e5d838d6d8520331554a6cf923e48
Vijay Krishnan <vijay_krishnan@woodberry.org>
2021-01-26 23:00:42 +0000

    updating new file

--- simplecalc.py
+++ simplecalc.py
@@ -23,10 +23,14 @@ if not input_number == "0":
     print("\nEnter number 2: ", end=" ")
     num2 = int(input())
 
-      if input_number == "1": print(f"\nsum of {num1} and {num2} is {num1 + num2}")
-    elif input_number == "2": print(f"\ndiffe between {num1} and {num2} is {num1 - num2}")
-    elif input_number == "3": print(f"\nProduct of {num1} and {num2} is {num1 * num2}")
-    elif input_number == "4": print(f"\n{num1} div by {num2} is {num1 / num2}")
+    if input_number == "1": 
+        print(f"\nsum of {num1} and {num2} is {num1 + num2}")
+    elif input_number == "2": 
+        print(f"\ndiffe between {num1} and {num2} is {num1 - num2}")
+    elif input_number == "3": 
+        print(f"\nProduct of {num1} and {num2} is {num1 * num2}")
+    elif input_number == "4": 
+        print(f"\n{num1} div by {num2} is {num1 / num2}")
     else:
         print("\nYou did not enter option 1, 2, 3 or 4")
 print("\n---------------------------------------------\n")
