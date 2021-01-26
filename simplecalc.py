51ac301a4c15fb70a7f68240a5630b1fd91264dc
Vijay Krishnan <vijay_krishnan@woodberry.org>
2021-01-26 21:14:23 +0000

    updating new file

--- simplecalc.py
+++ simplecalc.py
@@ -1,40 +1,32 @@
-print("Hi! Welcome to the calculator")
-
-print("\n\nWhat do you want to do?")
+# -------------------------------------------------------
+#. Engineering Class - Woodberry Forest
+#. VIjay Krishnan.   Python Assignment #1 - Decision
+#
+# Sample program to demonstrate printing, basic match calculations
+# -------------------------------------------------------
+print("----- Simple calculator app ----")
+print("\nWhat do you want to calculate?")
 print("""
-Addition - Press 1
-Subtraction - Press 2
-Multiplication - Press 3
-Division - Press 4
-Exit - Press 0
+Addition       - Press 1 Subtraction    - Press 2
+Multiplication - Press 3 Division       - Press 4
+Exit           - Press 0
 """)
 
-#Getting the operation to be performed
-operation = input().strip() #strip removes spaces before or after the input.
+input_number = input()
 
-#Checking if user wants to exit
-if not operation == "0":
+if not input_number == "0":
     #Getting the first number and converting from string to int
     print("\nEnter number 1: ", end=" ")
-    number_1 = int(input())
+    num1 = int(input())
 
     #Getting the second number and converting from string to int
     print("\nEnter number 2: ", end=" ")
-    number_2 = int(input())
+    num2 = int(input())
 
-    #Depending on user choice, we do the calculation and print the results
-    if operation == "1":
-        print(f"\nThe sum of {number_1} and {number_2} is {number_1 + number_2}")
-    elif operation == "2":
-        print(f"\nThe difference between {number_1} and {number_2} is {number_1 - number_2}")
-    elif operation == "3":
-        print((f"\nThe product of {number_1} and {number_2} is {number_1 * number_2}"))
-    elif operation == "4":
-        if not (number_2 == 0):
-            print((f"\n{number_1} divided by {number_2} is {number_1 / number_2}"))
-        else:
-            print("You cannot divide a number by 0")
+      if input_number == "1": print(f"\nsum of {num1} and {num2} is {num1 + num2}")
+    elif input_number == "2": print(f"\ndiffe between {num1} and {num2} is {num1 - num2}")
+    elif input_number == "3": print(f"\nProduct of {num1} and {num2} is {num1 * num2}")
+    elif input_number == "4": print(f"\n{num1} div by {num2} is {num1 / num2}")
     else:
         print("\nYou did not enter option 1, 2, 3 or 4")
-
-print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+print("\n---------------------------------------------\n")
