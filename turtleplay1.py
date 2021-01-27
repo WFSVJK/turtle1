3acd23d7e60092311cdac777d6b05738a05088f6
Vijay Krishnan <Vijay_krishnan@woodberry.org>
2021-01-27 22:37:14 +0000

    Adding turtle1 play

--- turtleplay1.py
+++ turtleplay1.py
@@ -5,16 +5,30 @@
 # Sample program to draw different objects
 # -------------------------------------------------------
 import turtle
-turtle.shape("turtle")
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
+#----------------differnt functions to draw differnt shapes -----
+def draw_square ():
+    print ("printing square")
+    turtle.shape("turtle")
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    print ("enter to continue")
+    input ()
+def draw_triangle ():
+    print ("printing Triangle")
+    
+def draw_circle ():
+    print ("printing circle")
+def draw_star ():
+    print ("printing star")
 
+
+#---------------------------------------------------------------
 print("----- Draw different Shapes ----")
 print("""
 Square         - Press 1 Triangle    - Press 2
@@ -29,14 +43,18 @@ if not input_number == "0":
     num1 = int(input())
     if input_number == "1": 
         print(f"\nprint sequre")
+        draw_square ()
     elif input_number == "2": 
         print(f"\nprint triangle")
+        draw_triangle ()
     elif input_number == "3": 
-        print(f"\nprint triangle 3")
+        print(f"\nprint circle 3")
+        draw_circle ()
     elif input_number == "4": 
         print(f"\nprint triangle4")
+        draw_star ()
     else:
         print("\nYou did not enter option 1, 2, 3 or 4")
-print("\n---------------------------------------------\n")
-
+else:
+    print("\n---------------------------------------------\n")
 
